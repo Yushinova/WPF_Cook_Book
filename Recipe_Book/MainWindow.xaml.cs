e05b01f141fe1e0248541daf265f599a24276bbc
Yushi_np28@top-academy.ru <Yushi_np28@top-academy.ru>
2024-01-27 00:15:02 +0300

    halabuda

--- Recipe_Book/MainWindow.xaml.cs
+++ Recipe_Book/MainWindow.xaml.cs
@@ -23,17 +23,54 @@ namespace Recipe_Book
     public partial class MainWindow : Window
     {
         public string Path;
+        public List <Section> sections = new List <Section> ();
         public MainWindow()
         {
             InitializeComponent();
         }
+        private void Serial()//for нужен и установить ID
+        {
+            string name = (((RecipeList.Items[0] as ListViewItem).Content as StackPanel).Children[1] as TextBlock).Text;
+            string id = (RecipeList.Items[0] as ListViewItem).Uid;
+            Section section = new Section { Name = name, ID = id };
+            sections.Add(section);
+            Text1.Text = sections[0].ID;
 
+
+        }
         private void Plus_Click(object sender, RoutedEventArgs e)
         {
             //FrameworkElement parent = (FrameworkElement)((Button)sender).Parent;
             Button b = sender as Button;
             //FrameworkElement parent2 = (FrameworkElement)(parent).Parent;
             Text1.Text = b.Uid;
+        }
+        private Button AddPlus(string text)
+        {
+            Button button = new Button();
+            Image img = new Image();
+            img.Source = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory.ToString() + @"image\4.png"));
+            button.Content = img;
+            button.Uid = text;
+            button.Click += new RoutedEventHandler(Plus_Click);
+            return button;
+        }
+        private StackPanel AddPanel(string path, string nameSection)
+        {
+            Image image = new Image();//вынести в отдельную функцию для динамического добавления
+            image.Width = 50;
+            image.Height = 50;
+            image.Source = new BitmapImage(new Uri(path));
+            image.Stretch = Stretch.Fill;  
+            StackPanel panel = new StackPanel();
+            TextBlock text = new TextBlock();
+            text.Text = nameSection;
+            Text1.Text = text.Text;
+            panel.Children.Add(image);
+            panel.Children.Add(text);
+            panel.Children.Add(AddPlus(text.Text));
+            //panel.Uid = Path.GetFileNameWithoutExtension(path);
+            return panel;
         }
         private void AddImage_Click(object sender, RoutedEventArgs e)
         {
@@ -55,32 +92,23 @@ namespace Recipe_Book
                 Text1.Text = Path;
                 // Path = dialog.FileName;
             }
-            Image image = new Image();//вынести в отдельную функцию для динамического добавления
-            image.Width = 50;
-            image.Height = 50;
-            image.Source = new BitmapImage(new Uri(Path));
-            image.Stretch = Stretch.Fill;
             Button b = sender as Button;
+            string nameSection = ((b.Parent as System.Windows.Controls.StackPanel).Children[1] as TextBox).Text;
+            string Hash = nameSection.GetHashCode().ToString();
             Text1.Text = b.Content.ToString();
-            StackPanel panel = new StackPanel();
-            TextBlock text = new TextBlock();
-            text.Text = ((b.Parent as System.Windows.Controls.StackPanel).Children[1] as TextBlock).Text;
-            Text1.Text = text.Text;
-            panel.Children.Add(image);
-            panel.Children.Add(text);
-            Button button = new Button();
-            Image img = new Image();
-            img.Source = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory.ToString() + @"image\4.png"));
-            button.Content = img;
-            button.Uid = text.Text;
-            button.Click+= new RoutedEventHandler(Plus_Click);
-            panel.Children.Add(button);
+            string save_name = Guid.NewGuid().ToString();//уникальный идентификатор
+            BitmapImage save = new BitmapImage(new Uri(Path));
+            JpegBitmapEncoder jpegBitmapEncoder = new JpegBitmapEncoder();//сохранение картинки в папке программы
+            jpegBitmapEncoder.Frames.Add(BitmapFrame.Create(save));
+            using (FileStream fileStream = new FileStream(@"image\" + save_name+ ".png", FileMode.Create))//bin debug
+                jpegBitmapEncoder.Save(fileStream);
             RecipeList.Items.RemoveAt(RecipeList.Items.Count - 1);
-            RecipeList.Items.Add(panel);
+            RecipeList.Items.Add(AddPanel(Path,nameSection));
+            Serial();
+
         }
         private void NewSection_Click(object sender, RoutedEventArgs e)
         {
-            string s = AppDomain.CurrentDomain.BaseDirectory.ToString() + @"image\1.png";//для примера берем картинку из папки
             StackPanel panel = new StackPanel();
             Button button = new Button();
             button.Height = 50;
@@ -89,12 +117,12 @@ namespace Recipe_Book
             button.Click += new RoutedEventHandler(AddImage_Click);//добавляем событие на кнопку
            
             panel.Children.Add(button);
-            TextBlock block = new TextBlock();
-            
-            //block.Height = 50;
-            //block.Width = 80;
-            block.Text = "Мой раздел";
-            panel.Children.Add(block);
+            TextBox box = new TextBox();
+            box.Height = 34;
+            box.Width = 160;
+            box.FontSize = 18;
+            box.Text = "Добавить";
+            panel.Children.Add(box);
             RecipeList.Items.Add(panel);
         }
     }
---
+++ Recipe_Book/Section.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recipe_Book
+{
+    public class Section
+    {
+        public string ID {  get; set; }
+        public string Name { get; set; }
+    }
+}
